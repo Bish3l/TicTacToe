961ee8833769318257df9142f06f769f0e1a01e9
victor bishel <matodorrrrr@gmail.com>
2023-05-23 16:02:21 +0700

    game is able to process and restart

---
+++ src/main/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GameGrid from './components/GameGrid';
+import './index.css';
+
+function App() {
+  return (
+    <div className="main-container">
+      <GameGrid/>
+    </div>
+  )
+}
+
+export default App;
